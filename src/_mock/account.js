689f051c02800d72b6f7a45724893d6fda0f2e48
Nico Cruz <NicoCruz1015@users.noreply.github.com>
2023-08-16 01:11:12 +0800

    try lang

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Nico Cruz',
+  displayName: 'Keano Piad',
   email: 'cruz.nico.c.7226@gmail.com',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
