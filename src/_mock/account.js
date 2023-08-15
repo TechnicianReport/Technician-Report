b5abd450eed5f33112e0b2346fdc9b75fb9a880b
Nico Cruz <NicoCruz1015@users.noreply.github.com>
2023-08-16 01:08:26 +0800

    try

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,8 +1,8 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
+  displayName: 'Nico Cruz',
+  email: 'cruz.nico.c.7226@gmail.com',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
 
