923a3935bb23e9bd6ab149f510e56f886b52ee1a
Nico Cruz <NicoCruz1015@users.noreply.github.com>
2023-08-15 23:56:59 +0800

    first Commit

---
+++ src/layouts/dashboard/nav/config.js
@@ -0,0 +1,53 @@
+// component
+import SvgColor from '../../../components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/dashboard/app',
+    icon: icon('ic_analytics'),
+  },
+ 
+  {
+    title: 'Forms',
+    path: '/dashboard/products',
+    icon: icon('ic_cart'),
+  },
+  {
+    title: 'Profiling',
+    path: '/dashboard/blog',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'Reports',
+    path: '/dashboard/products',
+    icon: icon('ic_lock'),
+  },
+  {
+    title: 'Archives',
+    path: '/dashboard/products',
+    icon: icon('ic_cart'),
+  },
+  {
+    title: 'login',
+    path: '/login',
+    icon: icon('ic_lock'),
+  },
+  // {
+  //   title: 'Not found',
+  //   path: '/404',
+  //   icon: icon('ic_disabled'),
+  // },
+ 
+  {
+    title: 'user',
+    path: '/dashboard/user',
+    icon: icon('ic_user'),
+  },
+];
+
+export default navConfig;
